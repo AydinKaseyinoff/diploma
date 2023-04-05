c0e9d309ae744b72ec76d078a1f0f4e6a8311ae2
AydinKaseyinoff <kaseyinov@gmail.com.com>
2023-04-05 15:51:31 +0600

    add components Home and About and put in content

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { Link, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
   return (
     <div className="App">
       <Layout>
         <Routes>
-          <Route path="/" element={<h1>Home</h1>} />
-          <Route path="/about" element={<h1>about</h1>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
           <Route path="/contacts" element={<h1>Contactcs</h1>} />
           <Route path="/delivery" element={<h1>Delivery</h1>} />
         </Routes>
---
+++ src/pages/About.js
@@ -0,0 +1,8 @@
+export default function About() {
+    return (
+        <div className="About">
+            <h1>About</h1>
+            <p>Hello my name is Aydin</p>
+        </div>
+    )
+}
