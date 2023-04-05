7ec292bc95dcef2a37ecd74af6844b90f05defbc
AydinKaseyinoff <kaseyinov@gmail.com.com>
2023-04-05 13:16:31 +0600

    add local files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { Link, Route, Routes } from "react-router-dom";
 
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+
+      <Routes>
+        <Route path="/" element={<h2>Home page</h2>} />
+      </Routes>
     </div>
   );
 }
