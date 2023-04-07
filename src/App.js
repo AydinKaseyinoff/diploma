7b2ed7dc9f80230568cda78d13a7ceb70f25ba8d
AydinKaseyinoff <kaseyinov@gmail.com.com>
2023-04-07 14:12:49 +0600

    fix bugs

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Link, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Contacts from "./pages/Contacts";
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/contacts" element={<h1>Contactcs</h1>} />
+          <Route path="/contacts" element={<Contacts />} />
           <Route path="/delivery" element={<h1>Delivery</h1>} />
         </Routes>
       </Layout>
---
+++ src/pages/Contacts.js
@@ -0,0 +1,9 @@
+export default function Contacts() {
+    return (
+        <div className="Contacts">
+            <h1>
+                <a tell="+996708338944">Tell</a>
+            </h1>
+        </div>
+    )
+}
