724ee6d3c4c049bfdc67487e7eb68c77452be6e9
AydinKaseyinoff <kaseyinov@gmail.com.com>
2023-04-12 00:27:17 +0600

    changed footer and categorylist styles

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,18 @@
-export default function Home() {
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Home.css";
+
+const HomePage = () => {
   return (
-    <div className="Home">
-      <h1>Home</h1>
+    <div>
+      <h1>Добро пожаловать в наш интернет-магазин аксессуаров для телефонов!</h1>
+      <p>Мы предлагаем широкий ассортимент аксессуаров для всех типов телефонов.</p>
+      <p>У нас вы можете найти чехлы, защитные стекла, зарядные устройства, наушники, кабели и многое другое.</p>
+      <Link to="/category">
+        <button>Посмотреть все товары</button>
+      </Link>
     </div>
   );
-}
+};
+
+export default HomePage;
