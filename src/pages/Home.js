9affa632211d44f3b13c3fc42eee2fc4d9cf465c
AydinKaseyinoff <kaseyinov@gmail.com.com>
2023-05-04 13:51:14 +0600

    change home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,11 @@
+import "./Home.css"
 export default function Home() {
   return (
     <div className="Home">
-      <h1>Home</h1>
+      <marquee behavior="scroll" direction="left"><img src="https://content2.rozetka.com.ua/banner_main/image/original/330997636.jpg" /></marquee>
+      <marquee behavior="scroll" direction="right"><h1>Этот текст будет прокручиваться слева направо</h1></marquee>
+      <marquee behavior="scroll" direction="up">Этот текст будет перемещаться вверх</marquee>
+      <marquee behavior="scroll" direction="down">Этот текст будет перемещаться вверх</marquee>
     </div>
   );
 }
