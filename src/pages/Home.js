5360faa2b8a1908fd1dc1f3bf90a803f40c04444
AydinKaseyinoff <kaseyinov@gmail.com.com>
2023-05-04 14:05:56 +0600

    fix home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import "./Home.css"
 export default function Home() {
   return (
     <div className="Home">
-      <marquee behavior="scroll" direction="left"><img src="https://content2.rozetka.com.ua/banner_main/image/original/330997636.jpg" /></marquee>
+      <h1 className="text">"Site under construction"</h1>
     </div>
   );
 }
