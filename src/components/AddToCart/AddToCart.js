595c4086ff5e0ddaf9ac43cff26beb4707491370
AydinKaseyinoff <kaseyinov@gmail.com.com>
2023-05-07 14:30:01 +0600

    add notification to addtocart and add style

--- src/components/AddToCart/AddToCart.js
+++ src/components/AddToCart/AddToCart.js
@@ -1,9 +1,10 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import "./AddToCart.css";
 import { AppContext } from "../../App";
 
 export default function AddToCart({ product }) {
   const { cart, setCart } = useContext(AppContext);
+  const [showNotification, setShowNotification] = useState(false);
 
   function onAddToCartClick() {
     const qty = cart[product.id] ? cart[product.id] + 1 : 1;
@@ -11,13 +12,23 @@ export default function AddToCart({ product }) {
       ...cart,
       [product.id]: qty
     });
+    setShowNotification(true);
+    setTimeout(() => setShowNotification(false), 2000);
   }
+
   return (
     <div className="AddToCart">
       <button onClick={onAddToCartClick}>
         <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/cd-cart.svg" alt="CART" />
         Add To Cart
       </button>
+      {showNotification && (
+        <div className="Notification">
+          <img src="https://cdn-icons-png.flaticon.com/512/1442/1442912.png" alt="CHECKMARK" />
+          <p>Товар успешно добавлен в корзину</p>
+        </div>
+      )}
     </div>
   );
 }
+
