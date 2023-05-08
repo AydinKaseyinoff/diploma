4f7ab76662e574a297b05c3d5212e4d89925bf0c
AydinKaseyinoff <kaseyinov@gmail.com.com>
2023-05-09 01:00:06 +0600

    change notfound page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -3,6 +3,7 @@ export default function NotFound() {
     <div className="NotFound">
       <h1>404</h1>
       <h2>Sorry, we couldn't find the page you were looking for.</h2>
+      <img src="https://i.pinimg.com/originals/3f/fb/77/3ffb779195c6c1c95fa6a87824b0562d.gif" alt="404" />
     </div>
   );
 }
