846f7e4df293ef2bf25a25bcc7c0b9f91c84a0af
AydinKaseyinoff <kaseyinov@gmail.com.com>
2023-05-12 15:33:18 +0600

    add image to ThankYou page

--- src/pages/ThankYou.js
+++ src/pages/ThankYou.js
@@ -3,6 +3,7 @@ export default function ThankYou() {
     return (
       <div className="ThankYou">
         <h1>Thank you!</h1>
+        <img src="https://github.com/AydinKaseyinoff/CoffeeShopFigma/blob/main/Mw7aT.gif?raw=true" alt="thank you" />
         <p>Your order has been received. Our specialists will contact with you shortly to confirm it.</p>
       </div>
     );
