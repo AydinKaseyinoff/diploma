402aaf7c48bbb552a34a16c859e33cfe0f2a8ef0
AydinKaseyinoff <kaseyinov@gmail.com.com>
2023-05-13 01:17:35 +0600

    add burger-menu

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,29 @@
 import Nav from "../Nav/Nav";
 import "./Layout.css";
 import CategoryList from "../CategoryList/CategoryList";
+import NavToggle from "../NavToggle/NavToggle";
+import Drawer from "../Drawer/Drawer";
+import { useState } from "react";
 
 export default function Layout(props) {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  function toggleDrawer() {
+    setDrawerOpen(!drawerOpen);
+  }
   return (
     <div className="Layout">
       <header>
         <Nav />
+        <NavToggle callback={toggleDrawer} />
+        <Drawer open={drawerOpen} toggle={toggleDrawer} />
       </header>
       <aside className="aside">
-        <img className="in-img" src="https://cdn.onlinewebfonts.com/svg/download_221204.png" alt="category" />
+        <img
+          className="in-img"
+          src="https://cdn.onlinewebfonts.com/svg/download_221204.png"
+          alt="category"
+        />
         <CategoryList className="CategoryList" />
       </aside>
       <main>{props.children}</main>
