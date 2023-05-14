632dee704312dc094814d7edc3acd3465d807cc1
AydinKaseyinoff <kaseyinov@gmail.com.com>
2023-05-14 14:38:22 +0600

    add modal window to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,14 @@
 import React from "react";
+import ModalWindow from "../components/ModalWindow/ModalWindow";
+import CategoryList from "../components/CategoryList/CategoryList";
 
 function Home() {
   return (
     <div>
       <h1>Welcome to our store!</h1>
       <h1>Site under constuction</h1>
+      <h1>Last updated <mark>14/05/23</mark> </h1>
+      <ModalWindow />
     </div>
   );
 }
