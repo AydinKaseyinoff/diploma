a001bfaa9d964f16d8d1779d24f89fac86ee3372
AydinKaseyinoff <kaseyinov@gmail.com.com>
2023-05-16 00:14:07 +0600

    change order list

--- src/components/OrderList/OrderList.js
+++ src/components/OrderList/OrderList.js
@@ -23,8 +23,8 @@ export default function OrderList() {
       return (
         <div key={product.id}>
           <img src={product.picture} alt={product.name} />
-          {product.name}: {order.cart[productId]} X {product.price} som ={" "}
-          {order.cart[productId] * product.price} som
+          {product.name}: {order.cart[productId]} X {product.price} ₽ ={" "}
+          {order.cart[productId] * product.price} ₽
         </div>
       );
     });
