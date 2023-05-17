c4c9f7eb9d39f4ab5957be0f2cfc1bfd35d772b1
AydinKaseyinoff <kaseyinov@gmail.com.com>
2023-05-17 11:21:26 +0600

    add atribut"loading-eager" for img from thank you page

--- src/pages/ThankYou.js
+++ src/pages/ThankYou.js
@@ -3,7 +3,7 @@ export default function ThankYou() {
     return (
       <div className="ThankYou">
         <h1>Thank you!</h1>
-        <img loading="eager" src="https://github.com/AydinKaseyinoff/CoffeeShopFigma/blob/main/Mw7aT.gif?raw=true" alt="thank you" />
+        <img src="https://github.com/AydinKaseyinoff/CoffeeShopFigma/blob/main/Mw7aT.gif?raw=true" alt="thank you" />
         <p>Your order has been received. Our specialists will contact with you shortly to confirm it.</p>
       </div>
     );
