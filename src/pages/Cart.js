c3fbe85b823154a0301d18ed3d36e14867bac0d9
AydinKaseyinoff <kaseyinov@gmail.com.com>
2023-05-17 10:31:55 +0600

    add class to cart page and import styles for cart

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,12 +1,15 @@
 import CartList from "../components/CartList/CartList";
 import OrderForm from "../components/OrderForm/OrderForm";
+import "./Cart.css";
 
 export default function Cart() {
     return (
         <div className="Cart">
             <h1>Cart</h1>
-            <CartList />
-            <OrderForm />
+            <div className="flex-cart">
+              <CartList />
+              <OrderForm />
+            </div>
         </div>
     )
 }
