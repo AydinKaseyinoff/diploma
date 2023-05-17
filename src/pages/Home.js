9ce0553eb35c9cc35a53deb361ee5a4ead327e3b
AydinKaseyinoff <kaseyinov@gmail.com.com>
2023-05-17 10:56:36 +0600

    add class"Home" to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Home() {
   return (
-    <div>
+    <div className="Home">
       <h1>Welcome to our store!</h1>
       <h1>Site under constuction</h1>
       <h1>
