835c8d62a59be6e5c8af7e10f8c6b337e8a832dd
AydinKaseyinoff <kaseyinov@gmail.com.com>
2023-05-18 00:20:21 +0600

    change img for thank you page and change styles product item

--- src/pages/ThankYou.js
+++ src/pages/ThankYou.js
@@ -3,7 +3,7 @@ export default function ThankYou() {
     return (
       <div className="ThankYou">
         <h1>Thank you!</h1>
-        <img src="https://github.com/AydinKaseyinoff/CoffeeShopFigma/blob/main/Mw7aT.gif?raw=true" alt="thank you" />
+        <img loading="eager" src="https://cdn.dribbble.com/users/1751799/screenshots/5512482/check02.gif" alt="thank you" />
         <p>Your order has been received. Our specialists will contact with you shortly to confirm it.</p>
       </div>
     );
