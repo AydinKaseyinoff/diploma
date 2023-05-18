1c281950671487464b0b4358e4481f1405210612
AydinKaseyinoff <kaseyinov@gmail.com.com>
2023-05-18 18:05:24 +0600

    change thank you page

--- src/pages/ThankYou.js
+++ src/pages/ThankYou.js
@@ -3,7 +3,7 @@ export default function ThankYou() {
     return (
       <div className="ThankYou">
         <h1>Thank you!</h1>
-        <img loading="eager" src="https://cdn.dribbble.com/users/1751799/screenshots/5512482/check02.gif" alt="thank you" />
+        <img loading="eager" src="https://cdn.dribbble.com/users/423514/screenshots/2770345/animated-check.gif" alt="thank you" />
         <p>Your order has been received. Our specialists will contact with you shortly to confirm it.</p>
       </div>
     );
