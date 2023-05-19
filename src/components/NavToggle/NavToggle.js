50db3edb84239d551b008e5c41606c383227f8d8
AydinKaseyinoff <kaseyinov@gmail.com.com>
2023-05-19 20:51:38 +0600

    add styles"@media" to Nav Toggle for adaptability

--- src/components/NavToggle/NavToggle.js
+++ src/components/NavToggle/NavToggle.js
@@ -1,11 +1,19 @@
+import { useContext } from "react";
 import "./NavToggle.css";
+import { AppContext } from "../../App";
 
 function NavToggle({ callback }) {
+  const { cart } = useContext(AppContext);
+
+  const total = Object.values(cart).reduce((acc, num) => acc + num, 0);
   return (
     <button onClick={callback} className="NavToggle">
       <div className="line"></div>
       <div className="line"></div>
       <div className="line"></div>
+      <div className="total">
+        {total > 0 ? total : null}
+      </div>
     </button>
   );
 }
