c7cab2acfc8e5ade5a8665541d54f4c021205a08
AydinKaseyinoff <kaseyinov@gmail.com.com>
2023-05-19 22:56:34 +0600

    change disain web

--- src/components/CartLink/CartLink.js
+++ src/components/CartLink/CartLink.js
@@ -14,7 +14,9 @@ export default function CartLink() {
                 <div>
                   <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/cd-cart.svg" alt="CART" />
                 </div>
-                {total}
+                <div className="total-cart">
+                  {total > 0 ? total : null}
+                </div>
             </NavLink>
         </div>
     )
