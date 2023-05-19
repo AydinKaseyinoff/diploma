65c6f421df356d153efea492f837f627fa5dfe1c
AydinKaseyinoff <kaseyinov@gmail.com.com>
2023-05-19 23:31:08 +0600

    add modal window"categories" to than you page

--- src/pages/ThankYou.js
+++ src/pages/ThankYou.js
@@ -1,3 +1,4 @@
+import ModalWindow from "../components/ModalWindow/ModalWindow";
 import "./ThankYou.css"
 export default function ThankYou() {
     return (
@@ -5,6 +6,9 @@ export default function ThankYou() {
         <h1>Thank you!</h1>
         <img loading="eager" src="https://cdn.dribbble.com/users/423514/screenshots/2770345/animated-check.gif" alt="thank you" />
         <p>Your order has been received. Our specialists will contact with you shortly to confirm it.</p>
+        <div>
+          to <ModalWindow />
+        </div>
       </div>
     );
   }
