04d39b5b494bd845db9d6bac7b7088d3209787a6
AydinKaseyinoff <kaseyinov@gmail.com.com>
2023-05-20 23:41:00 +0600

    add props to cart link burger

--- src/components/CartLink/CartLink-Burger.js
+++ src/components/CartLink/CartLink-Burger.js
@@ -3,19 +3,28 @@ import "./CartLink-Burger.css";
 import { AppContext } from "../../App";
 import { NavLink } from "react-router-dom";
 
-export default function CartLinkBurger() {
-    const { cart } = useContext(AppContext);
+export default function CartLinkBurger(props) {
+  const { cart } = useContext(AppContext);
 
-    const total = Object.values(cart).reduce((acc, num) => acc + num, 0);
+  const total = Object.values(cart).reduce((acc, num) => acc + num, 0);
 
-    return (
-        <div className="CartLink-Burger">
-            <NavLink to="/cart">
-                <div>
-                  <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/cd-cart.svg" alt="CART" />
-                </div>
-                {total}
-            </NavLink>
+  const handleNavigationClick = () => {
+    if (props.onNavigationClick) {
+      props.onNavigationClick();
+    }
+  };
+
+  return (
+    <div className="CartLink-Burger">
+      <NavLink to="/cart" onClick={handleNavigationClick}>
+        <div>
+          <img
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/cd-cart.svg"
+            alt="CART"
+          />
         </div>
-    )
+        {total}
+      </NavLink>
+    </div>
+  );
 }
