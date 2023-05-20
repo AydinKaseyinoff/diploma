4e020408ebda3820f86ec71495a8a7dd839c9f06
AydinKaseyinoff <kaseyinov@gmail.com.com>
2023-05-21 00:00:50 +0600

    change support page

--- src/components/SupportPage/SupportPage.js
+++ src/components/SupportPage/SupportPage.js
@@ -1,10 +1,14 @@
-import React, { useState } from 'react';
-import Input from './Input';
-import Button from './Button';
+import React, { useState } from "react";
+import Input from "./Input";
+import Button from "./Button";
 import "./SupportPage.css";
 
 function SupportPage() {
-  const [formData, setFormData] = useState({ name: '', email: '', message: '' });
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -14,27 +18,31 @@ function SupportPage() {
   return (
     <div>
       <h1>Свяжитесь с нами</h1>
-      <form className='Support' onSubmit={handleSubmit}>
+      <form className="Support" onSubmit={handleSubmit}>
         <Input
           label="Имя"
           name="name"
           value={formData.name}
-          onChange={(event) => setFormData({ ...formData, name: event.target.value })}
+          onChange={(event) =>
+            setFormData({ ...formData, name: event.target.value })
+          }
         />
         <Input
           label="Email"
           name="email"
           type="email"
           value={formData.email}
-          onChange={(event) => setFormData({ ...formData, email: event.target.value })}
+          onChange={(event) =>
+            setFormData({ ...formData, email: event.target.value })
+          }
         />
-        <Input
-          label="Сообщение"
-          name="message"
-          as="textarea"
-          value={formData.message}
-          onChange={(event) => setFormData({ ...formData, message: event.target.value })}
+        <textarea
+          rows={4} // Количество видимых строк
+          cols={50} // Количество видимых символов в строке// Значение текстовой области
+          placeholder="Введите текст" // Плейсхолдер (подсказка для ввода)
+          disabled={false} // Отключение текстовой области
         />
+
         <Button type="submit">Отправить</Button>
       </form>
     </div>
