12456d389e3a8d192c40dc11b69e0f9185db7d2e
AydinKaseyinoff <kaseyinov@gmail.com.com>
2023-05-24 09:44:06 +0600

    add recomended product to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,37 @@
-import React from "react";
+import React, { useContext } from "react";
+import { NavLink } from "react-router-dom";
+import { AppContext } from "../App";
 
 function Home() {
+  const { products } = useContext(AppContext);
+
+  // Генерируем случайное число в диапазоне от 0 до длины массива продуктов
+  const randomIndexes = [];
+  while (randomIndexes.length < 3) {
+    const randomIndex = Math.floor(Math.random() * products.length);
+    if (!randomIndexes.includes(randomIndex)) {
+      randomIndexes.push(randomIndex);
+    }
+  }
+
+  // Получаем случайные продукты по сгенерированным индексам
+  const randomProducts = randomIndexes.map((index) => products[index]);
+
   return (
     <div className="Home">
       <h1>Welcome to our store!</h1>
-      <h1>Site under constuction</h1>
-      <h1>
-        Last updated <mark>19/05/23</mark>
-      </h1>
-      <iframe
-        src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d34649.10289465772!2d78.70511999296602!3d42.545190545721624!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sky!2skg!4v1684312077766!5m2!1sky!2skg"
-        width="100%"
-        height="450"
-        style={{border:0}}
-        allowfullscreen=""
-        loading="lazy"
-        referrerpolicy="no-referrer-when-downgrade"
-        title="Google maps"
-      ></iframe>
+      <h2>Featured Products:</h2>
+      <div className="ProductList">
+        {randomProducts.map((product) => (
+          <div key={product.id} className="BoxList">
+            <img src={product.picture} alt={product.name} />
+            <NavLink to={"/products/" + product.slug}>{product.name}</NavLink>
+            <div>
+              <span>{product.price} ₽</span>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
