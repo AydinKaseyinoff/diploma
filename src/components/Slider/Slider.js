55102ef94b34dbbcacf4cf55985e4e9f653f6388
AydinKaseyinoff <kaseyinov@gmail.com.com>
2023-05-24 20:18:02 +0600

    change go to next slider time"3500"

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -24,7 +24,7 @@ export default function Slider() {
   }, [images.length]);
 
   useEffect(() => {
-    const interval = setInterval(goToNextImage, 3000); // Измените интервал времени по вашему выбору (в данном случае - 3 секунды)
+    const interval = setInterval(goToNextImage, 3500); // Измените интервал времени по вашему выбору (в данном случае - 3 секунды)
     return () => {
       clearInterval(interval);
     };
